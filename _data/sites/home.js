17ec7f90e5843c5a1cba1f8bf5c6bd918988eead
Frankie <26727138+frankievalentine@users.noreply.github.com>
2023-06-22 13:40:54 -0700

    Update sites and removed guide message on layout

---
+++ _data/sites/home.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: "Homepage", // optional, falls back to object key
+	description: "Sites",
+	options: {
+		runs: 1,
+		frequency: 10, // (in minutes)
+	},
+	urls: [
+		"https://www.v3frankie.me/"
+	]
+};
